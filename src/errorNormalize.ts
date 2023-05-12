3b5d2f7acefe2dc31e20f936728f836458211729
#云淡然 <hi@ydr.me>
2023-05-12 15:45:11 +0800

    docs: 使用短链接

--- src/errorNormalize.ts
+++ src/errorNormalize.ts
@@ -2,3 +2,5 @@ export function errorNormalize(error: unknown): Error {
   if (error && error instanceof Error) return error;
   return new Error(String(error ?? ''));
 }
+
+export { errorNormalize as normalizeError };
